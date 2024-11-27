202120352e2b9ab65db36710b8b28381f86b7852
ulomaonyekwere <munaonyekwere@gmail.com>
2024-11-27 16:50:34 +0100

    Add files via upload

--- practice_1/target/debug/practice_1.d
+++ practice_1/target/debug/practice_1.d
@@ -1,1 +1,1 @@
-/Users/macbookair/m.onyekwereCSC101/week-3/practice_1/target/debug/practice_1: /Users/macbookair/m.onyekwereCSC101/week-3/practice_1/src/main.rs
+/Users/macbookair/m.onyekwereCSC101/week_6/practice_1/target/debug/practice_1: /Users/macbookair/m.onyekwereCSC101/week_6/practice_1/src/main.rs
--- practice_3/target/debug/practice_3.d
+++ practice_3/target/debug/practice_3.d
@@ -1,1 +1,1 @@
-/Users/macbookair/m.onyekwereCSC101/week-3/practice_2/practice_3/target/debug/practice_3: /Users/macbookair/m.onyekwereCSC101/week-3/practice_2/practice_3/src/main.rs
+/Users/macbookair/m.onyekwereCSC101/week_6/practice_3/target/debug/practice_3: /Users/macbookair/m.onyekwereCSC101/week_6/practice_3/src/main.rs
