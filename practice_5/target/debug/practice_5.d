6c39f0d47cc8212ad3cfc0495512e4a189744b23
ulomaonyekwere <munaonyekwere@gmail.com>
2024-12-08 23:31:40 +0100

    Add files via upload

--- practice_5/target/debug/practice_5.d
+++ practice_5/target/debug/practice_5.d
@@ -1,1 +1,1 @@
-/Users/macbookair/m.onyekwereCSC101/week_6/practice_5/target/debug/practice_5: /Users/macbookair/m.onyekwereCSC101/week_6/practice_5/src/main.rs
+/Users/macbookair/m.onyekwereCSC101/week_8/practice_5/target/debug/practice_5: /Users/macbookair/m.onyekwereCSC101/week_8/practice_5/src/main.rs
